591b1f8263fca1c470f80d0e162aafd2fee6ee35
Conan <98342638+ZYMNZ@users.noreply.github.com>
2023-08-25 12:09:33 -0400

    Create Sample.dart

---
+++ lib/Lab1/Sample.dart
@@ -0,0 +1,128 @@
+
+
+import 'dart:io';
+
+void main(){
+  // print("Hello World");
+  // var name = "Peter";
+  // var roll_no = 24;
+  // print("My name is ${name} My roll number is ${roll_no}");
+
+  // var heart_symbol = "\u2665";
+  // var laugh_symnbol = "\u{1f600}";
+  // print(heart_symbol);
+  // print(laugh_symnbol);
+
+
+  // final name = "Ricky";
+  // final String msg = "How are you";
+  // const a = 1000;
+  //
+  // print("Example of Assignment operation");
+  // var n1 = 10;
+  // var n2 = 5;
+  //
+  // print("n1 + n2 = ${n1 + n2}");
+  // print("n1 - n2 = ${n1 - n2}");
+  // print("n1 * n2 = ${n1 * n2}");
+  // print("n1 / n2 = ${n1 / n2}");
+  // print("n1 % n2 = ${n1 % n2}");
+  //
+  // int r = 5;
+  // double pi = 3.14;
+  // double res = 4 * pi * r * r;
+  // print("The are of sphere = ${res}");
+
+  ///////////////
+  // as typecast
+  // is it returns true if the obj has specified type
+  // is! ... false
+
+  var num = 5;
+
+
+
+  // String str1 = 'This is an example of a single-line string';
+  // String str2 = "this is an example of a double-quotes multiline line string";
+  // String str3 = '''this is an example of a
+  // double-quotes multiline line string''';
+
+  // var a = 10;
+  // var b = 10;
+  //
+  // print(str1);
+  // print(str2);
+  // print(str3);
+  //
+  // print("The sim is ${a + b}");
+
+
+  // list
+  // var list1 = <int>[];
+  // list1.add(10);
+  // list1.add(10);
+  // list1.add(11);
+  // list1.add(12);
+  // list1.add(12);
+  // list1.add(13);
+  // list1.add(14);
+  // print(list1);
+
+  //set ...it removes duplicates "Unique"
+  // var mySet = <String>{};
+  //
+  // mySet.add("apple");
+  // mySet.add("banana");
+  // mySet.add("orange");
+  // mySet.add("apple");
+  //
+  // print(mySet);
+  //
+  // print(mySet.contains("banana"));
+  // print(mySet.contains("grape"));
+  //
+  // mySet.remove("apple");
+  // print(mySet);
+
+  // MAP
+
+  // var myMap = <String, int>{};
+  //
+  // myMap["apple"] = 3;
+  // myMap["banana"] = 5;
+  // myMap["orange"] = 4;
+  //
+  // print(myMap);
+  //
+  // print(myMap["banana"]);
+  //
+  // print(myMap.containsKey("apple"));
+  // print(myMap.containsValue(3));
+  //
+  // myMap.remove("apple");
+  // print(myMap);
+
+//factorial of 5
+  int f = 1;
+  for(int i =1; i <=5; i++){
+    f *=i;
+  }
+  print(f);
+
+
+// even or odd
+  int number = 20;
+  var num1 = 33;
+
+  if(num1 % 2 == 0){
+    print("even");
+  }
+  else
+    print("odd");
+
+// largest num
+//....
+
+
+}
+
