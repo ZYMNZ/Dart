a2d07b0c78b1a629cdd4b8fa9b8171841c587257
Conan <98342638+ZYMNZ@users.noreply.github.com>
2023-11-19 14:43:51 -0500

    Add files via upload

---
+++ lib/lab_22/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Map(),
+    );
+  }
+}
+
+class Map extends StatelessWidget {
+  const Map({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "Streetmap Demo"
+        ),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+            initialCenter: LatLng(45.4178665,-73.467187),
+            initialZoom: 15.0
+        ),
+        children: [
+          TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.animation'
+          ),
+        ],
+      ),
+    );
+  }
+}
